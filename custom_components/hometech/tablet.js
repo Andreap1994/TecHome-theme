5eaf7b8a136d62c071edacfce9e692649781eed7
Andreap1994 <41423294+Andreap1994@users.noreply.github.com>
2025-06-14 20:33:08 +0200

    Delete custom_components/hometech directory

--- custom_components/hometech/tablet.js
+++
@@ -1,8 +0,0 @@
-class TuoDashboard extends HTMLElement {
-  setConfig(config) {
-    // Configurazione del Dashboard
-    this.innerHTML = `<h1>${config.title}</h1>`;
-  }
-}
-
-customElements.define('tuo-dashboard', TuoDashboard);
